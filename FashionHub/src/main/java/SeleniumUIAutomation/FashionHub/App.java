9e6a41268984a66ef314901640af2d85b03e5308
Saransh <saransh.saxena18@gmail.com>
2025-07-26 19:52:37 +0530

    Create a new Maven Project FashionHub

---
+++ FashionHub/src/main/java/SeleniumUIAutomation/FashionHub/App.java
@@ -0,0 +1,13 @@
+package SeleniumUIAutomation.FashionHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
