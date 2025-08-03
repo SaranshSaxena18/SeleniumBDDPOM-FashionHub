7f1dcaf0c2b27c113ef22378263d79476967b261
Saransh <saransh.saxena18@gmail.com>
2025-08-04 01:55:01 +0530

    added test data file and data provider

--- FashionHub/src/main/java/Utilities/Utility.java
+++ FashionHub/src/main/java/Utilities/Utility.java
@@ -1,7 +1,14 @@
 package Utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
@@ -9,6 +16,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.*;
+import org.testng.annotations.DataProvider;
 
 public class Utility {
 	WebDriver driver;
@@ -67,5 +75,37 @@ public class Utility {
 			System.out.println("Unable to the click the element after waiting for 50 seconds - "+e.getMessage());
 		}
 		
+	}
+
+	DataFormatter formatter = new DataFormatter();
+	@DataProvider(name="TestData")
+	public Object[][] getDataFromExcel()throws IOException
+	{	
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//Resources//TestData//TestData.xlsx");//path of the test data file
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);//creating the object of XSSFWorkbook to load the xlsx sheet so that it can be interacted with.
+		int numberOfSheets = workbook.getNumberOfSheets();//to get the total number of sheets in the excel file
+		for(int i=0;i<numberOfSheets;i++)//run the for loop till the total number of sheets
+		{
+			if(workbook.getSheetAt(i).getSheetName().equalsIgnoreCase("testdata"))
+			{
+				XSSFSheet sheet = workbook.getSheetAt(i);//load the sheet
+				int rowCount = sheet.getPhysicalNumberOfRows();//get total number of rows
+				XSSFRow row = sheet.getRow(0);//get the first row
+				int colCount = row.getLastCellNum();//number of total columns
+				Object[][] data = new Object[rowCount-1][colCount];//rowCount-1 as first row is the heading
+				for(int j=1;j<rowCount;j++)//run the loop till total number of rows,  j=1 as first row is heading
+				{
+					XSSFRow row1 = sheet.getRow(j);
+					if(row1 == null) continue;
+					for(int k=0;k<colCount;k++)
+					{
+						XSSFCell cell = row1.getCell(k);
+						data[j-1][k] = formatter.formatCellValue(cell);
+					}
+				}
+				return data;
+			}
+		}
+		return null;
 	}
 }
--- FashionHub/src/test/java/Runner/TestRunner.java
+++ FashionHub/src/test/java/Runner/TestRunner.java
@@ -1,7 +1,12 @@
 package Runner;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 
 @CucumberOptions(
 		 	features = "src/main/java/Features",
@@ -10,4 +15,15 @@ import io.cucumber.testng.CucumberOptions;
 		)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-}
+//	 @Override
+//	    @DataProvider(parallel = false)
+//	    public Object[][] scenarios() {
+//	        return super.scenarios();
+//	    }
+//
+//	    @Test(dataProvider = "TestData")
+//	    public void runScenario(PickleWrapper pickle, FeatureWrapper cucumberFeature) {
+//	        // Your step definitions will execute scenarios one by one
+//	        // To pass external DataProvider data into steps, consider using dependency injection or hooks.
+//	    }
+	}
