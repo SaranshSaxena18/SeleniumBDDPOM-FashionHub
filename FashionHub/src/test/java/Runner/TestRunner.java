30afd8fef06d554dd856f8a39c255b61dff11cc3
SaranshSaxena18 <saransh.saxena18@gmail.com>
2025-09-02 23:08:59 +0530

    updated testRunner file

--- FashionHub/src/test/java/Runner/TestRunner.java
+++ FashionHub/src/test/java/Runner/TestRunner.java
@@ -19,9 +19,9 @@ import io.cucumber.testng.*;
 public class TestRunner extends AbstractTestNGCucumberTests 
 {
 
-	 @Override
-	    @DataProvider(parallel = true)
-	    public Object[][] scenarios() {
-	        return super.scenarios();
-	    }
+//	 @Override
+//	    @DataProvider(parallel = true)
+//	    public Object[][] scenarios() {
+//	        return super.scenarios();
+//	    }
 	}
